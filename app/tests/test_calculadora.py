383e8236fd1cb258c4536130f336227c404599c9
Helena Oliveira <helena.oliveira@casaldev.com.br>
2021-09-16 16:40:01 -0300

    add more tests

--- app/tests/test_calculadora.py
+++ app/tests/test_calculadora.py
@@ -8,10 +8,10 @@ except:
 
 import unittest
 
-from calculadora import soma
+from calculadora import soma, subtrai
 
 
-class TestCalculadora(unittest.TestCase):
+class TestCalculadoraSoma(unittest.TestCase):
     def test_soma_5_e_5_deve_retornar_10(self):
         self.assertEqual(soma(5, 5), 10)
 
@@ -39,6 +39,36 @@ class TestCalculadora(unittest.TestCase):
     def test_soma_y_nao_e_int_ou_float_deve_retornar_assertionerror(self):
         with self.assertRaises(AssertionError):
             soma(11, "0")
+
+
+class TestCalculadoraSubtrai(unittest.TestCase):
+    def test_subtrai_5_e_5_deve_retornar_10(self):
+        self.assertEqual(subtrai(5, 5), 0)
+
+    def test_subtrai_5_negativo_e_5_deve_retornar_0(self):
+        self.assertEqual(subtrai(-5, 5), -10)
+
+    def test_subtrai_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 0),
+            (5, 10, -5),
+            (3.5, 1.5, 2.0),
+            (-5, 5, -10),
+            (100, 50, 50),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(subtrai(x, y), saida)
+
+    def test_subtrai_x_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            subtrai("11", 0)
+
+    def test_subtrai_y_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            subtrai(11, "0")
 
 
 if __name__ == "__main__":
